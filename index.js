7e6fdc778268f19953d57f41116531cc1153fbb2
creamy-dev <56458201+creamy-dev@users.noreply.github.com>
2022-03-02 17:01:12 -0500

    feature: Adds Linux support.

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
 import lib from './lib.js';
 
 if (Deno.build.os === "linux") {
-    console.log(await lib.existsSync(await lib.join(lib.getHomeDir(), "AppData", "Local", "Concrete", "Concrete-win32-x64", "Concrete.exe")));
-} else if (Deno.build.os !== "windows") {
-    console.log("We do not currently support " + Deno.build.os + ".");
-    Deno.exit(1);
-} else {
+    if (await lib.existsSync(await lib.join(lib.getHomeDir(), ".concrete", "Concrete-linux-x64", "Concrete"))) {
+        console.log("Concrete already exists! Attempting to kill Concrete...");
+        await lib.runShell("killall Concrete");
+        console.log("Deleting Concrete...");
+        try {
+            await Deno.remove(await lib.join(lib.getHomeDir(), ".concrete"), { recursive: true });
+        } catch (e) {
+            if (e.includes("Error: The directory is not empty. (os error 145)")) {
+                console.error("Concrete is still running. Please close Concrete and try again.");
+                Deno.exit(1);
+            } else {
+                throw e;
+            }
+        }
+    }
+
+    console.log("Creating Concrete directories...");
+
+    try {
+        await Deno.mkdir(await lib.join(lib.getHomeDir(), ".concrete"), { recursive: true });
+    } catch (e) {
+        console.error("Error creating Concrete directory! " + e);
+        Deno.exit(1);
+    }
+
+    console.log("Downloading Concrete...");
+
+    try {
+        let data = await lib.get("https://api.github.com/repos/ashxi/project-kilo/releases/latest");
+        let parsedData = JSON.parse(data);
+        let url = "";
+
+        for (let asset of parsedData.assets) {
+            if (asset.name.startsWith("Concrete-linux-x64")) {
+                url = asset.browser_download_url;
+                break;
+            }
+        }
+
+        await lib.download(url, await lib.join(lib.tempDir(), "Concrete.zip"));
+    } catch (e) {
+        console.error("Error downloading Concrete! " + e);
+        Deno.exit(1);
+    }
+
+    console.log("Fetched Concrete.zip. Extracting...");
+
+    await Deno.writeTextFile(await lib.join(lib.tempDir(), "ConcreteInstall"), `#!/bin/bash\nunzip "${await lib.join(lib.tempDir(), "Concrete.zip")}" -d "${await lib.join(lib.getHomeDir(), ".concrete")}"`);
+    await lib.runShell(`chmod +x ${await lib.join(lib.tempDir(), "ConcreteInstall")}`);
+    await lib.runShell(await lib.join(lib.tempDir(), "ConcreteInstall"));
+    console.log("Concrete has been installed!");
+} else if (Deno.build.os == "windows") {
     if (await lib.existsSync(await lib.join(lib.getHomeDir(), "AppData", "Local", "Concrete", "Concrete-win32-x64", "Concrete.exe"))) {
         console.log("Concrete already exists! Attempting to kill Concrete...");
         await lib.runShell("taskkill /f /im Concrete.exe");
@@ -72,4 +119,7 @@ if (Deno.build.os === "linux") {
     await Deno.writeTextFile(await lib.join(lib.tempDir(), "ConcreteShortcut.bat"), `@echo off\ncscript "${await lib.join(lib.tempDir(), "ConcreteShortcut.vbs")}"`);
     await lib.runShell(await lib.join(lib.tempDir(), "ConcreteShortcut.bat"));
     console.log("Concrete shortcut has been created.");
-}   
+} else {
+    console.log("We do not currently support " + Deno.build.os + ".");
+    Deno.exit(1);
+}
